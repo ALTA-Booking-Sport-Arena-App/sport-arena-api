4d99881bbbb9620fa08fdd9a9ae074093a2d284e
husnulnawafil <husnulnawafil27@gmail.com>
2022-04-29 05:17:55 +0700

    fix: user endpoints response

--- delivery/handler/user/user.go
+++ delivery/handler/user/user.go
@@ -32,11 +32,11 @@ func (uh *UserHandler) CreateUserHandler() echo.HandlerFunc {
 
 		errBind := c.Bind(&param)
 		if errBind != nil {
-			return c.JSON(http.StatusInternalServerError, helper.ResponseFailed("Error binding data"))
+			return c.JSON(http.StatusBadRequest, helper.ResponseFailed("Error binding data"))
 		}
 		_, err := uh.userUseCase.CreateUser(param)
 		if err != nil {
-			return c.JSON(http.StatusInternalServerError, helper.ResponseFailed("Register failed"))
+			return c.JSON(http.StatusBadRequest, helper.ResponseFailed("Register failed"))
 		}
 		return c.JSON(http.StatusOK, helper.ResponseSuccessWithoutData("Successfully registered"))
 	}
@@ -48,20 +48,20 @@ func (uh *UserHandler) DeleteUserHandler() echo.HandlerFunc {
 
 		idToken, errToken := _middlewares.ExtractToken(c)
 		if errToken != nil {
-			return c.JSON(http.StatusUnauthorized, helper.ResponseFailed("Unauthorized"))
+			return c.JSON(http.StatusUnauthorized, helper.ResponseFailed("unauthorized"))
 		}
 
 		userId, _ := strconv.Atoi(c.Param("userId"))
 
 		if idToken != userId {
-			return c.JSON(http.StatusUnauthorized, helper.ResponseFailed("Unauthorized"))
+			return c.JSON(http.StatusUnauthorized, helper.ResponseFailed("unauthorized"))
 		}
 
 		err := uh.userUseCase.DeleteUser(userId)
 		if err != nil {
-			return c.JSON(http.StatusInternalServerError, helper.ResponseFailed(err.Error()))
+			return c.JSON(http.StatusBadRequest, helper.ResponseFailed("failed delete user"))
 		}
-		return c.JSON(http.StatusOK, helper.ResponseSuccess("Successfully deleted", err))
+		return c.JSON(http.StatusOK, helper.ResponseSuccessWithoutData("success delete user"))
 	}
 }
 
@@ -72,25 +72,25 @@ func (uh *UserHandler) UpdateUserHandler() echo.HandlerFunc {
 		// check login status
 		idToken, errToken := _middlewares.ExtractToken(c)
 		if errToken != nil {
-			return c.JSON(http.StatusUnauthorized, helper.ResponseFailed("Unauthorized"))
+			return c.JSON(http.StatusUnauthorized, helper.ResponseFailed("unauthorized"))
 		}
 
 		userId, _ := strconv.Atoi(c.Param("userId"))
 		// check authorization
 		if idToken != userId {
-			return c.JSON(http.StatusUnauthorized, helper.ResponseFailed("Unauthorized"))
+			return c.JSON(http.StatusUnauthorized, helper.ResponseFailed("unauthorized"))
 		}
 		// binding request data
 		err := c.Bind(&updateRequest)
 		if err != nil {
-			return c.JSON(http.StatusInternalServerError, helper.ResponseFailed(err.Error()))
+			return c.JSON(http.StatusBadRequest, helper.ResponseFailed("failed edit user profile"))
 		}
 		_, rows, err := uh.userUseCase.UpdateUser(userId, updateRequest)
 		if err != nil {
-			return c.JSON(http.StatusInternalServerError, helper.ResponseFailed(err.Error()))
+			return c.JSON(http.StatusBadRequest, helper.ResponseFailed("failed edit user profile"))
 		}
 		if rows == 0 {
-			return c.JSON(http.StatusBadRequest, helper.ResponseFailed("User not found"))
+			return c.JSON(http.StatusBadRequest, helper.ResponseFailed("user not found"))
 		}
 		users, _, _ := uh.userUseCase.GetUserById(userId)
 		responseUser := map[string]interface{}{
@@ -99,7 +99,7 @@ func (uh *UserHandler) UpdateUserHandler() echo.HandlerFunc {
 			"email": users.Email,
 		}
 
-		return c.JSON(http.StatusOK, helper.ResponseSuccess("Success update user data", responseUser))
+		return c.JSON(http.StatusOK, helper.ResponseSuccess("success edit user profile", responseUser))
 	}
 }
 
@@ -108,42 +108,42 @@ func (uh *UserHandler) UpdateUserImageHandler() echo.HandlerFunc {
 		// check login status
 		idToken, errToken := _middlewares.ExtractToken(c)
 		if errToken != nil {
-			return c.JSON(http.StatusUnauthorized, helper.ResponseFailed("Unauthorized"))
+			return c.JSON(http.StatusUnauthorized, helper.ResponseFailed("unauthorized"))
 		}
 		userId, _ := strconv.Atoi(c.Param("userId"))
 		// check authorization
 		if idToken != userId {
-			return c.JSON(http.StatusUnauthorized, helper.ResponseFailed("Unauthorized"))
+			return c.JSON(http.StatusUnauthorized, helper.ResponseFailed("unauthorized"))
 		}
 		// binding image
 		fileData, fileInfo, err_binding_image := c.Request().FormFile("image")
 		if err_binding_image != nil {
-			return c.JSON(http.StatusBadRequest, helper.ResponseFailed("Error to bind image"))
+			return c.JSON(http.StatusBadRequest, helper.ResponseFailed("error to bind image"))
 		}
 		// check file CheckFileExtension
 		_, err_check_extension := image.CheckImageFileExtension(fileInfo.Filename)
 		if err_check_extension != nil {
-			return c.JSON(http.StatusBadRequest, helper.ResponseFailed("Error checking file extension"))
+			return c.JSON(http.StatusBadRequest, helper.ResponseFailed("error checking file extension"))
 		}
 		// check file size
 		err_check_size := image.CheckImageFileSize(fileInfo.Size)
 		if err_check_size != nil {
-			return c.JSON(http.StatusBadRequest, helper.ResponseFailed("Error checking file size"))
+			return c.JSON(http.StatusBadRequest, helper.ResponseFailed("error checking file size"))
 		}
 		fileName := "user_profile_id_" + strconv.Itoa(idToken)
 		// upload the image
 		theUrl, err_upload_photo := image.UploadImage("users", fileName, fileData)
 		if err_upload_photo != nil {
-			return c.JSON(http.StatusBadRequest, helper.ResponseFailed("Error to upload file"))
+			return c.JSON(http.StatusBadRequest, helper.ResponseFailed("error to upload file"))
 		}
 		rows, err := uh.userUseCase.UpdateUserImage(theUrl, idToken)
 		if err != nil {
-			return c.JSON(http.StatusInternalServerError, helper.ResponseFailed(err.Error()))
+			return c.JSON(http.StatusBadRequest, helper.ResponseFailed("failed edit user profile"))
 		}
 		if rows == 0 {
-			return c.JSON(http.StatusBadRequest, helper.ResponseFailed("User not found"))
+			return c.JSON(http.StatusBadRequest, helper.ResponseFailed("user not found"))
 		}
-		return c.JSON(http.StatusOK, helper.ResponseSuccessWithoutData("Success to update user image"))
+		return c.JSON(http.StatusOK, helper.ResponseSuccessWithoutData("success edit user profile"))
 	}
 }
 
@@ -173,9 +173,9 @@ func (uh *UserHandler) GetUserProfile() echo.HandlerFunc {
 		}
 
 		if err != nil {
-			return c.JSON(http.StatusInternalServerError, helper.ResponseFailed("Get user profile failed"))
+			return c.JSON(http.StatusBadRequest, helper.ResponseFailed("failed get user profile"))
 		}
-		return c.JSON(http.StatusOK, helper.ResponseSuccess("Successfully get user profile", responseUser))
+		return c.JSON(http.StatusOK, helper.ResponseSuccess("success get user profile", responseUser))
 	}
 }
 
@@ -184,13 +184,13 @@ func (uh *UserHandler) RequestOwnerHandler() echo.HandlerFunc {
 		// check login status
 		idToken, errToken := _middlewares.ExtractToken(c)
 		if errToken != nil {
-			return c.JSON(http.StatusUnauthorized, helper.ResponseFailed("Unauthorized"))
+			return c.JSON(http.StatusUnauthorized, helper.ResponseFailed("unauthorized"))
 		}
 		// binding data
 		var requestOwner _entities.User
 		errBind := c.Bind(&requestOwner)
 		if errBind != nil {
-			return c.JSON(http.StatusInternalServerError, helper.ResponseFailed("error to bind data"))
+			return c.JSON(http.StatusBadRequest, helper.ResponseFailed("error to bind data"))
 		}
 		// binding certiifcate
 		fileData, fileInfo, err_binding_certificate := c.Request().FormFile("business_certificate")
@@ -200,30 +200,30 @@ func (uh *UserHandler) RequestOwnerHandler() echo.HandlerFunc {
 		// check file CheckFileExtension
 		_, err_check_extension := certificate.CheckCertificateFileExtension(fileInfo.Filename)
 		if err_check_extension != nil {
-			return c.JSON(http.StatusBadRequest, helper.ResponseFailed("Please upload pdf file"))
+			return c.JSON(http.StatusBadRequest, helper.ResponseFailed("please upload pdf file"))
 		}
 		// check file size
 		err_check_size := certificate.CheckCertificateFileSize(fileInfo.Size)
 		if err_check_size != nil {
-			return c.JSON(http.StatusBadRequest, helper.ResponseFailed("File is too big"))
+			return c.JSON(http.StatusBadRequest, helper.ResponseFailed("file is too big"))
 		}
 		fileName := "user_owner_certificate_" + strconv.Itoa(idToken)
 		// upload the certificate
 		certificate, err_upload_certificate := certificate.UploadCertificate("owners", fileName, fileData)
 		if err_upload_certificate != nil {
-			return c.JSON(http.StatusBadRequest, helper.ResponseFailed("Error to upload file"))
+			return c.JSON(http.StatusBadRequest, helper.ResponseFailed("error to upload file"))
 		}
 		rows, err := uh.userUseCase.RequestOwner(idToken, certificate, requestOwner)
 		if err != nil {
-			return c.JSON(http.StatusInternalServerError, helper.ResponseFailed(err.Error()))
+			return c.JSON(http.StatusBadRequest, helper.ResponseFailed("failed to request for being owner"))
 		}
 		if rows == 0 {
-			return c.JSON(http.StatusBadRequest, helper.ResponseFailed("User not found"))
+			return c.JSON(http.StatusBadRequest, helper.ResponseFailed("user not found"))
 		}
 		if rows == -1 {
-			return c.JSON(http.StatusBadRequest, helper.ResponseFailed("Please fill all needed fields"))
+			return c.JSON(http.StatusBadRequest, helper.ResponseFailed("please fill all needed fields"))
 		}
-		return c.JSON(http.StatusOK, helper.ResponseSuccessWithoutData("Success to request for being owner"))
+		return c.JSON(http.StatusOK, helper.ResponseSuccessWithoutData("success to request for being owner"))
 	}
 }
 
@@ -245,7 +245,7 @@ func (uh *UserHandler) GetListUsersHandler() echo.HandlerFunc {
 		//call GetListUsers function
 		listUsers, err := uh.userUseCase.GetListUsers()
 		if err != nil {
-			return c.JSON(http.StatusInternalServerError, helper.ResponseFailed("failed to get all users"))
+			return c.JSON(http.StatusBadRequest, helper.ResponseFailed("failed to get all users"))
 		}
 		return c.JSON(http.StatusOK, helper.ResponseSuccess("success to get all users", listUsers))
 	}
@@ -269,7 +269,7 @@ func (uh *UserHandler) GetLIstOwnersHandler() echo.HandlerFunc {
 		//call GetListOwner function
 		listOwners, err := uh.userUseCase.GetListOwners()
 		if err != nil {
-			return c.JSON(http.StatusInternalServerError, helper.ResponseFailed("failed to get all owners"))
+			return c.JSON(http.StatusBadRequest, helper.ResponseFailed("failed to get all owners"))
 		}
 		return c.JSON(http.StatusOK, helper.ResponseSuccess("success to get all owners", listOwners))
 	}
@@ -294,11 +294,11 @@ func (uh *UserHandler) ApproveOwnerRequestHandler() echo.HandlerFunc {
 		// binding request data
 		errBind := c.Bind(&request)
 		if errBind != nil {
-			return c.JSON(http.StatusInternalServerError, helper.ResponseFailed(errBind.Error()))
+			return c.JSON(http.StatusBadRequest, helper.ResponseFailed(errBind.Error()))
 		}
 		err := uh.userUseCase.ApproveOwnerRequest(request)
 		if err != nil {
-			return c.JSON(http.StatusInternalServerError, helper.ResponseFailed("verification approved failed"))
+			return c.JSON(http.StatusBadRequest, helper.ResponseFailed("verification approved failed"))
 		}
 		return c.JSON(http.StatusOK, helper.ResponseSuccessWithoutData("verification approved successfully"))
 	}
@@ -323,11 +323,11 @@ func (uh *UserHandler) RejectOwnerRequestHandler() echo.HandlerFunc {
 		// binding request data
 		errBind := c.Bind(&request)
 		if errBind != nil {
-			return c.JSON(http.StatusInternalServerError, helper.ResponseFailed(errBind.Error()))
+			return c.JSON(http.StatusBadRequest, helper.ResponseFailed(errBind.Error()))
 		}
 		err := uh.userUseCase.ApproveOwnerRequest(request)
 		if err != nil {
-			return c.JSON(http.StatusInternalServerError, helper.ResponseFailed("verification reject failed"))
+			return c.JSON(http.StatusBadRequest, helper.ResponseFailed("verification reject failed"))
 		}
 		return c.JSON(http.StatusOK, helper.ResponseSuccessWithoutData("verification reject successfully"))
 	}
@@ -351,11 +351,11 @@ func (uh *UserHandler) UpdateAdminHandler() echo.HandlerFunc {
 		}
 		errBind := c.Bind(&request)
 		if errBind != nil {
-			return c.JSON(http.StatusInternalServerError, helper.ResponseFailed(errBind.Error()))
+			return c.JSON(http.StatusBadRequest, helper.ResponseFailed(errBind.Error()))
 		}
 		err := uh.userUseCase.UpdateAdmin(id, request)
 		if err != nil {
-			return c.JSON(http.StatusInternalServerError, helper.ResponseFailed("updated password failed"))
+			return c.JSON(http.StatusBadRequest, helper.ResponseFailed("updated password failed"))
 		}
 		return c.JSON(http.StatusOK, helper.ResponseSuccessWithoutData("updated password successfully"))
 	}
